e84e9ffb4925a63b7a774de0a39691ed55f4071c
spyer <sundayomoladee11@gmail.com>
2024-03-16 23:46:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var menuopen = document.getElementById("menuopen")
+var closemenu = document.getElementById("closemenu")
+var nav = document.getElementById("nav")
+menuopen.addEventListener("click",()=>{
+    nav.classList.add('active')
+    
+})
+closemenu.addEventListener('click',()=>{
+    nav.classList.remove("active")
+})
+document.addEventListener("click",(e)=>{
+    const target=e.target
+   
+    if (target !== menuopen && !menuopen.contains(target)){
+        nav.classList.remove("active")
+    }
+})
